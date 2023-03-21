85fc6d8726d9a583fa34f88c82249f8575fe11b9
Gautam Deorazz <gdeoprasad@gmail.com>
2023-03-21 16:07:47 +0530

    Save DayYearProblem

---
+++ src/StringHandler/Program_one.java
@@ -0,0 +1,18 @@
+package StringHandler;
+
+public class Program_one {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        String s3=new String("Java");
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append("Ja").append("va");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1 == s3);
+        System.out.println(s1.equals(s3));
+        System.out.println(sb1.toString() == s1);
+        System.out.println(sb1.toString().equals(s1));
+    }
+}
